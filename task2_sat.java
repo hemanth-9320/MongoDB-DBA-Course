25db2f0a8374e529b022e0a92a04e8b33f3e9d5e
hemanth-9320 <141582081+hemanth-9320@users.noreply.github.com>
2024-06-19 11:31:48 +0530

    finalsat

--- task2_sat.java
+++ task2_sat.java
@@ -1,13 +1,14 @@
 //TASK-2 SATURDAY
 
 //1st question- Create an employee collection with 6 or more documents having name,salary and age.
+//2nd question- Find documents having minimum salary and age is between 30 and 40.
 
 import java.util.ArrayList;
 import org.bson.Document;
 import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
 
-public class TASK1_SAT {
+public class EmployeeManagement {
     public static void main(String[] args) {
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("Company");
@@ -39,7 +40,7 @@ public class TASK1_SAT {
             .append("salary", 75000)
             .append("age", 40);
 
-        ArrayList<Document> documents = new ArrayList<Document>();
+        ArrayList<Document> documents = new ArrayList<>();
         documents.add(document1);
         documents.add(document2);
         documents.add(document3);
@@ -49,6 +50,7 @@ public class TASK1_SAT {
 
         collection.insertMany(documents);
 
+        // Finding the employee(s) with the highest salary
         Document highestSalaryDoc = collection.find().sort(new Document("salary", -1)).first();
         if (highestSalaryDoc != null) {
             int highestSalary = highestSalaryDoc.getInteger("salary");
@@ -60,6 +62,20 @@ public class TASK1_SAT {
             }
         } else {
             System.out.println("No employees found");
+        }
+
+        // Finding the employee with the minimum salary in the age range 30 to 40
+        FindIterable<Document> sortedBySalary = collection.find(Filters.and(
+            Filters.gte("age", 30),
+            Filters.lte("age", 40)
+        )).sort(new Document("salary", 1)).limit(1);
+
+        Document lowestSalary = sortedBySalary.first();
+        if (lowestSalary != null) {
+            System.out.println("Employee with the lowest salary in the age range 30 to 40:");
+            System.out.println(lowestSalary.toJson());
+        } else {
+            System.out.println("No employees found in the age range 30 to 40");
         }
 
         mongoClient.close();
